9663bec356d6ff5f0d146959fa8363de4e86b5a7
Alejandro Andrade <meuxdev@gmail.com>
2022-07-24 23:13:34 -0500

    Feature 2. Fixing eslint errors.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,7 @@
 import 'tailwindcss/tailwind.css';
 
-alert("No sense alert")
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
-  console.log("Random unnecessary log");
 }
 
 export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,7 @@
 export default function Home() {
-
-  const name = "Random inecesario Name"
-
   return (
     <>
-      <h1>Hello Next.js</h1
+      <h1>Hello Next.js</h1>
     </>
   );
 }
