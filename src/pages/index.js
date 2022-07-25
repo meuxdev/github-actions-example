5e2418982ce9729fd9bfe1a5cdc2a219b3323250
Alejandro Andrade <meuxdev@gmail.com>
2022-07-24 22:38:04 -0500

    Fix: /pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 export default function Home() {
-  const hola = "Hello";
-  const name = "Random AName koaskdfjba"
   return (
     <>
-   A   <h1>Hello Next.js</h1
+      <h1>Hello Next.js</h1>
     </>
-  )
+  );
 }
